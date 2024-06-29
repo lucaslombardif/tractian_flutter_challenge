dc3dc9deb62607a5a815a6200ee957d6e1048e40
lucaslombardif <lucaas-floriano@hotmail.com>
2024-06-29 01:12:46 -0300

    feat: created project structure

---
+++ lib/shared/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primaryColor: const Color(0xFF0D0452),
+  appBarTheme: const AppBarTheme(
+    centerTitle: true,
+    backgroundColor: Color(0xFF0D0452),
+  ),
+);
